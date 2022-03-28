2f5cacea3f89453872f963865e54ba215ff63695
Asmita-Nyoupane <74106020+Asmita-Nyoupane@users.noreply.github.com>
2022-03-28 06:20:19 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * plugin name: Greeting Template
+ * plugin URL:https://wordpress.org/plugins/
+ * Description : Simple greeting card which show your website more attractive
+ * <version:1 class="0"></version:1>
+ *author:Asmita Nyoupane
+ * author URL :https://www.facebook.com/ashmita.neupane.104
+ */
+add_action('init', 'my_custom_post_type');
+function my_custom_post_type{
+    register_post_type( string Greeting, array|string $args = array(
+        <label for="greeting tepmlate"></label>
+        public =>true,
+
+        ) )
+}
+
+?>
